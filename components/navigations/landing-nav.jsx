724c82aa879e0eb2daf2bd4bfbf197d157b823ee
enhasya <enhasya@outlook.co.id>
2024-03-01 18:39:59 +0700

    update

--- components/navigations/landing-nav.jsx
+++ components/navigations/landing-nav.jsx
@@ -8,6 +8,8 @@ const logo = {
 const icon = {
   Announcement: require("@/shared/icons/vuesax/outline/magic-star-black.svg"),
   Expand: require("@/shared/icons/vuesax/outline/more-blue.svg"),
+  ArrowRight: require("@/shared/icons/vuesax/outline/arrow-right-black.svg"),
+  ArrowUp: require("@/shared/icons/vuesax/outline/arrow-up-black.svg"),
 };
 
 const LandingNav = () => {
@@ -23,7 +25,11 @@ const LandingNav = () => {
   }, []);
 
   return (
-    <nav className="bg-[#F8F8FF]/10 lg:fixed top-0 z-10 flex flex-col lg:flex-row gap-8 lg:gap-4 items-center justify-between w-full h-auto backdrop-blur-lg p-8 lg:px-32 lg:py-4">
+    <nav
+      className={`bg-[#F8F8FF]/10 lg:fixed top-0 z-10 flex flex-col lg:flex-row gap-8 lg:gap-4 items-center justify-between w-full ${
+        expandLandingNav ? "h-screen lg:h-auto" : "h-auto"
+      } backdrop-blur-lg p-8 lg:px-32 lg:py-4`}
+    >
       <div className="flex flex-col lg:flex-row gap-8 lg:gap-0 items-start lg:items-center justify-center lg:justify-start w-full lg:w-[40%]">
         <div className="flex flex-row gap-2 items-center justify-between w-full lg:w-auto h-auto mt-0">
           <button className="bg-transparent transition-all flex flex-row gap-2 items-center rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
@@ -49,20 +55,23 @@ const LandingNav = () => {
         </div>
         {expandLandingNav && (
           <React.Fragment>
-            <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
+            <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center justify-between rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
               <p className="font-medium text-sm tracking-tight text-start mt-0">
                 UI Kit
               </p>
+              <Image className="flex lg:hidden" src={icon.ArrowRight} width="16" height="16" />
             </button>
-            <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
+            <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center justify-between rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
               <p className="font-medium text-sm tracking-tight text-start mt-0">
                 Pricing
               </p>
+              <Image className="flex lg:hidden" src={icon.ArrowRight} width="16" height="16" />
             </button>
-            <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
+            <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center justify-between rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
               <p className="font-medium text-sm tracking-tight text-start mt-0">
                 Terms of Use
               </p>
+              <Image className="flex lg:hidden" src={icon.ArrowRight} width="16" height="16" />
             </button>
           </React.Fragment>
         )}
@@ -77,15 +86,17 @@ const LandingNav = () => {
       </div>
       {expandLandingNav && (
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-0 items-start lg:items-end justify-center lg:justify-end w-full lg:w-[40%]">
-          <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
+          <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center justify-between rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
             <p className="font-medium text-sm tracking-tight text-start mt-0">
               Contact
             </p>
+            <Image src={icon.ArrowUp} width="16" height="16" />
           </button>
-          <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
+          <button className="bg-transparent text-black transition-all hover:text-[#3000E6] lg:hover:text-black lg:hover:bg-white flex flex-row gap-2 items-center justify-between rounded-lg w-full lg:w-auto p-0 lg:px-4 lg:py-2">
             <p className="font-medium text-sm tracking-tight text-start mt-0">
               Support
             </p>
+            <Image src={icon.ArrowUp} width="16" height="16" />
           </button>
         </div>
       )}
